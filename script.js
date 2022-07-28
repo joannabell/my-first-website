8a74c9d9521373c4d178e4ea7ba8854dac4dc6e0
Joanna Bell <jjbdigital@gmail.com>
2022-07-28 13:19:45 -0500

    basic calc

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var num1 = window.prompt("Enter a number.");
 var num2 = window.prompt("Enter another number");
 
-document.write( num1 + num2 )
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+document.write( num1 + num2 );
