4913d67e6700da5b17c74c87c6e403b04f05154d
Joanna Bell <jjbdigital@gmail.com>
2022-07-28 13:05:09 -0500

    age

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var name = window.prompt("What is your name?");
 var age = window.prompt("How old are you?");
 
 document.write("Hey, " + name + " how are you today?")
+document.write("Hey, " + name + " happy birthday!")
