3a8734b03188acc862a7a0e75a287173075b2382
Joanna Bell <jjbdigital@gmail.com>
2022-07-28 13:23:02 -0500

    fruits array

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-var num1 = window.prompt("Enter a number.");
-var num2 = window.prompt("Enter another number");
-
-num1 = parseFloat(num1);
-num2 = parseFloat(num2);
-
-document.write( num1 + num2 );
+var fruits = new Array("Apples", "Oranges", "Peaches");
+document.write( fruits );
