01cba721f72f21197eabfe333b526ddd02bce26f
Joanna Bell <jjbdigital@gmail.com>
2022-07-29 10:46:12 -0500

    added second parameter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
- function sayHi(name) {
+ function sayHi(name, age) {
     document.write("<h1>Hello " + name + "</h1>");
+    document.write("<p>You are " + age +"years old</p>")
  }
- sayHi("Jo");
- sayHi("Buttons");
+ sayHi("Jo", 45);
+ sayHi("Buttons", 4);
